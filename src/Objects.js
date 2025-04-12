d73e2148ef207929f982f4fcc750a5bde30696c7
Звард Геворгова <zvard.gevorgova@yandex.ru>
2025-04-12 23:04:33 +0300

    Задание objects

--- src/Objects.js
+++ src/Objects.js
@@ -8,6 +8,16 @@
   Объект после манипуляций следует вернуть в качестве результата работы функции.
 */
 export function personUpdate(data) {
+    if (data.gender === 'female') {
+        if ('age' in data) {
+            delete data.age;
+        }
+    } else if (data.gender === 'male') {
+        if (!('income' in data)) {
+            data.income = 100000;
+        }
+    }
+    return data;
 }
 
 /*
@@ -15,6 +25,21 @@ export function personUpdate(data) {
   Верните список названий этих полей в алфавитном порядке в виде массива строк.
 */
 export function objectFieldsList(obj1, obj2, obj3) {
+    const fields = new Set();
+
+    for (const key in obj1) {
+        fields.add(key);
+    }
+
+    for (const key in obj2) {
+        fields.add(key);
+    }
+
+    for (const key in obj3) {
+        fields.add(key);
+    }
+
+    return Array.from(fields).sort();
 }
 
 /*
@@ -22,5 +47,17 @@ export function objectFieldsList(obj1, obj2, obj3) {
   При этом каждый клон должен дополнительно содержать поле id со своим порядковым номером в массиве.
   Количество клонов - count.
 */
+/*
+  Верните в результате работы функции массив с клонами объекта obj.
+  При этом каждый клон должен дополнительно содержать поле id со своим порядковым номером в массиве.
+  Количество клонов - count.
+*/
 export function objectClone(obj, count) {
+    const clones = [];
+    for (let i = 0; i < count; i++) {
+        const clone = JSON.parse(JSON.stringify(obj));
+        clone.id = i;
+        clones.push(clone);
+    }
+    return clones;
 }
